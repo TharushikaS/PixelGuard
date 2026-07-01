47457af28fc533bd598544f2f53ba6ba051f8d99
Dinuth Diruksha <dinuthk69@gmail.com>
2026-07-01 21:45:14 +0530

    cm

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/encode': 'http://localhost:8000',
-      '/decode': 'http://localhost:8000'
+      '/encode': 'http://127.0.0.1:8000',
+      '/decode': 'http://127.0.0.1:8000'
     }
   }
 })
